fc6008d5292e0ffe31e6bec81886bb10c76ce8b6
josiahhaswell <josiah.haswell@gmail.com>
2013-09-23 12:53:55 -0700

    Added support for Arquillian integration testing

---
+++ model/src/test/common/java/org/sequoia/model/core/ModelTestHarness.java
@@ -0,0 +1,11 @@
+package org.sequoia.model.core;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: josiahhaswell
+ * Date: 9/23/13
+ * Time: 11:53 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ModelTestHarness {
+}
