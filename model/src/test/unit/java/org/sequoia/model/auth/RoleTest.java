d2db3eb2b5e896f66695951dc3e5df3341182bf1
josiahhaswell <josiah.haswell@gmail.com>
2013-09-23 17:24:47 -0700

    Added support for roles and associated tests

---
+++ model/src/test/unit/java/org/sequoia/model/auth/RoleTest.java
@@ -0,0 +1,11 @@
+package org.sequoia.model.auth;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: josiahhaswell
+ * Date: 9/23/13
+ * Time: 4:58 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class RoleTest {
+}
